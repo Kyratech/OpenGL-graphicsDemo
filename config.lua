fa2600fa8d9009d048c6befed7c6a0cfe61af82b
Mattadon <ml2g14@soton.ac.uk>
2017-02-26 16:21:45 +0000

    Added config file

---
+++ config.lua
@@ -0,0 +1,15 @@
+--
+-- Coursework configuration
+--
+-- Name: Matthew Langford, ml2g14
+--
+print("Config.lua executed")
+GLFW = GLFW3
+GLew = true
+IMAGE = 'stb_image'
+STD = 'c++11'
+BOOST = false
+BULLET = false
+MODEL = true
+GUI = 'ImGui'
+FILES = {'**.cpp'}
